0ce3d311e0d698e7a7fb1e018fa0d1cc917ff702
samanthamaphake <166666715+samanthamaphake@users.noreply.github.com>
2024-08-06 08:31:55 +0200

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,4 +1,11 @@
 //create a function 
-function conatactForm(){
-    let 
+function contactForm(){
+    let FirstName = document.getElementById('Firstame').value;
+    let LastName = document.getElementById('LastName').value;
+    let Email = document.getElementById('Email').value;
+    let Phone = document.getElementById('Phone').value;
+
+    alert(`Hello + ${FirstName} + ${LastName} + ${Email} + ${Phone}:`);
+
 }
+contactForm()
